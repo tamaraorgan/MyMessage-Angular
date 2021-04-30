a328c8e1a8b4b28ed4e3f6102776744e9c5d0b32
Tamara Organ <tamara@organ.dev.br>
2021-04-30 10:43:58 -0400

    Update - My message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project-angular';
+  storedPosts = [];
+  onPostAdded(post) {
+    this.storedPosts.push(post)
+  }
 }
