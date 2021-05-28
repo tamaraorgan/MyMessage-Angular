539069839178b531de1a867e41d533eda69a7949
Tamara Organ <tamara@organ.dev.br>
2021-05-28 19:44:33 -0400

    teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { NgModule } from '@angular/core'
+
 import { FormsModule } from '@angular/forms'
 import { MatInputModule } from '@angular/material/input'
 import { MatCardModule } from '@angular/material/card'
@@ -13,7 +14,6 @@ import { PostCreateComponent } from './posts/post-create/post-create.component'
 import { HeaderComponent } from './header/header.component'
 import { PostListComponent } from './posts/post-list/post-list.component'
 import { FooterComponent } from './footer/footer.component'
-
 
 @NgModule({
   declarations: [
